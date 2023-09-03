23deb8e194b6b1f19beae29d02fb105a4acf8466
bluepeng9 <bluepeng9@gmail.com>
2023-09-04 00:34:05 +0900

    feat: session 로그인 추가

--- src/main/java/com/example/springlogin/SpringLoginApplication.java
+++ src/main/java/com/example/springlogin/SpringLoginApplication.java
@@ -2,8 +2,17 @@ package com.example.springlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+        basePackages = {"com.example"},
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.example.springlogin.member.controller.MemberController" +
+                        "&& !(com.example.springlogin.member.controller.MemberSessionController)")
+        }
+)
 public class SpringLoginApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/springlogin/member/controller/MemberSessionController.java
@@ -0,0 +1,79 @@
+package com.example.springlogin.member.controller;
+
+import com.example.springlogin.member.domain.Member;
+import com.example.springlogin.member.dto.MemberJoinRequest;
+import com.example.springlogin.member.dto.MemberLoginRequest;
+import com.example.springlogin.member.service.MemberJoinParam;
+import com.example.springlogin.member.service.MemberLoginParam;
+import com.example.springlogin.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MemberSessionController {
+
+    final MemberService memberService;
+
+    @GetMapping("/")
+    public String getHomePage(@SessionAttribute(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId != null) {
+            Member user = memberService.getUser(memberId);
+            model.addAttribute("email", user == null ? null : user.getEmail());
+        }
+
+        return "home";
+    }
+
+    @GetMapping("/join")
+    public String getJoinPage(Model model) {
+        model.addAttribute("memberJoinRequest", new MemberJoinRequest());
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String join(@ModelAttribute("memberJoinRequest") MemberJoinRequest request) {
+        log.info("request = {}", request);
+        MemberJoinParam param = MemberJoinParam.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .build();
+        memberService.join(param);
+        return "redirect:";
+    }
+
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        model.addAttribute("memberLoginRequest", new MemberLoginRequest());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("memberLoginRequest") MemberLoginRequest request, Model model, HttpServletRequest servletRequest) {
+        log.info("request = {}", request);
+
+        MemberLoginParam param = MemberLoginParam.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .build();
+
+        Member found = memberService.login(param);
+
+        HttpSession session = servletRequest.getSession(true);
+        session.setAttribute("memberId", found.getId());
+        session.setMaxInactiveInterval(60 * 10);
+
+        return "redirect:";
+    }
+}
