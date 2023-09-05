b03831218e8783c639c7ba19f5e59d91e98485c8
bluepeng9 <bluepeng9@gmail.com>
2023-09-06 01:38:14 +0900

    feat: 회원가입 시 비밀번호 암호화 기능 추가

--- src/main/java/com/example/springlogin/member/service/MemberService.java
+++ src/main/java/com/example/springlogin/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.example.springlogin.member.domain.Member;
 import com.example.springlogin.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,13 +17,15 @@ import java.util.Optional;
 public class MemberService {
 
     final MemberRepository memberRepository;
+    final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void join(MemberJoinParam param) {
         log.info("param = {}", param);
+
         Member member = Member.builder()
                 .email(param.email)
-                .password(param.password)
+                .password(passwordEncoder.encode(param.password))
                 .build();
 
         memberRepository.save(member);
