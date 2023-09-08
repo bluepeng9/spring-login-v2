0783f44aac02567039fa7126655002570b41a033
bluepeng9 <bluepeng9@gmail.com>
2023-09-08 22:44:13 +0900

    feat: UserDetailsImpl 추가

---
+++ src/main/java/com/example/springlogin/config/UserDetailsImpl.java
@@ -0,0 +1,53 @@
+package com.example.springlogin.config;
+
+import com.example.springlogin.member.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final Member member;
+
+    public UserDetailsImpl(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collections = new ArrayList<>();
+        return collections;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
