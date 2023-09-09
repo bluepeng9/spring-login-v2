223e3d56e93fc70d5b78ecf509249d8b652f6892
bluepeng9 <bluepeng9@gmail.com>
2023-09-09 23:36:05 +0900

    feat: UserDetailsServiceImpl 추가

---
+++ src/main/java/com/example/springlogin/config/UserDetailsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.springlogin.config;
+
+import com.example.springlogin.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    MemberService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new UserDetailsImpl(service.getUser(username));
+    }
+}
