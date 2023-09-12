5a1b0be33554d173ccabeb3b674ebb0fadb2c58d
bluepeng9 <bluepeng9@gmail.com>
2023-09-12 23:42:50 +0900

    feat: spring security config 설정

--- src/main/java/com/example/springlogin/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springlogin/config/SpringSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.springlogin.config;
 
+import com.example.springlogin.member.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,15 +13,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
         return http.authorizeHttpRequests(
-                        customizer -> customizer.anyRequest().permitAll()
+                        customizer -> customizer
+                                .anyRequest().permitAll()
                 )
+                .formLogin(config -> config
+                        .loginPage("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                )
+                .userDetailsService(userDetailsService)
                 .build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(MemberService service) {
+        return new UserDetailsServiceImpl(service);
     }
 }
