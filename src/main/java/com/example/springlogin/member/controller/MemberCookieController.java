c24ef9687f2f81fb93cce5d86cf155d17425cc78
bluepeng9 <bluepeng9@gmail.com>
2023-09-15 22:22:35 +0900

    refactor: MemberController 이름 변경

--- src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
+++ src/main/java/com/example/springlogin/member/controller/MemberCookieController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-public class MemberController {
+public class MemberCookieController {
 
     final MemberService memberService;
 
